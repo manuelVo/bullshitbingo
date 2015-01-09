b4cbb02276ab9414cd0002fff7d7a4224db40626
Manuel Vögele <develop@manuel-voegele.de>
2015-01-09 20:44:52 +0100

    Positioning the words in the grid

--- startgame.php
+++ startgame.php
@@ -14,8 +14,51 @@ $stmt->execute();
 $result = $stmt->get_result();
 $row = $result->fetch_assoc();
 
+$mysqli->autocommit(false);
+
 $stmt = $mysqli->prepare("UPDATE games SET started = 1 WHERE id = ? AND admin = ? AND size * size <= ?;");
 $stmt->bind_param("iii", $_SESSION["gameid"], $_SESSION["id"], $row["count"]);
 $stmt->execute();
+
+if ($stmt->affected_rows == 0)
+{
+	exit;
+}
+
+$stmt = $mysqli->prepare("SELECT size FROM games WHERE id = ?;");
+$stmt->bind_param("i", $_SESSION["gameid"]);
+$stmt->execute();
+$result = $stmt->get_result();
+$game = $result->fetch_assoc();
+
+$stmt = $mysqli->prepare("SELECT wordid FROM assigned_words WHERE gameid = ?;");
+$stmt->bind_param("i", $_SESSION["gameid"]);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$words = array();
+while ($row = $result->fetch_assoc())
+{
+	$words[] = $row["wordid"];
+}
+
+$stmt = $mysqli->prepare("SELECT id FROM players WHERE gameid = ?;");
+$stmt->bind_param("i", $_SESSION["gameid"]);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$stmt = $mysqli->prepare("INSERT INTO positioned_words (playerid, wordid, position) VALUES (?,?,?);");
+while ($row = $result->fetch_assoc())
+{
+	$playerwords = $words;
+	shuffle($playerwords);
+	for ($i = 0;$i < $game["size"] * $game["size"];$i++)
+	{
+		$stmt->bind_param("iii", $row["id"], $playerwords[$i], $i);
+		$stmt->execute();
+	}
+}
+
+$mysqli->commit();
 
 ?>
