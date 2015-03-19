c4483b34f6646886672bfc4589d56145779a2b04
Manuel Vögele <develop@manuel-voegele.de>
2015-03-19 17:11:11 +0100

    Increased session timeout to 10h

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php
-	ini_set('session.gc_maxlifetime', 3600);
-	session_set_cookie_params(3600);
+	ini_set('session.gc_maxlifetime', 36000);
+	session_set_cookie_params(36000);
 	session_start();
 	header('Content-Type: text/html; charset=utf-8');
 	// TODO: Add mechanism to remove games which are no logner used
